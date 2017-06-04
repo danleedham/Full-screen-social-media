bdb3c86936fd244a77f4efc4c6b9ac46b81353f7
Dan Leedham <danleedham@googlemail.com>
2017-06-04 22:09:41 +0100

    Initial commit

---
+++ public/js/dashboard.js
@@ -0,0 +1,60 @@
+var app = angular.module('StarterApp', ['ngRoute', 'LocalStorageModule', 'angularify.semantic', 'socket-io']);
+
+app.controller('AppCtrl', ['$scope', '$location',
+    function($scope, $location){
+        $scope.menu = [];
+
+        $scope.isActive = function (viewLocation) {
+            return viewLocation === $location.path();
+        };
+        
+    	$scope.menu.push({
+            name: 'Social Media',
+            url: '/social-media',
+            type: 'link',
+            icon: 'blue twitter',
+        });
+
+    }
+]);
+
+/*
+ *  Configure the app routes
+ */
+app.config(['$routeProvider', 'localStorageServiceProvider',
+    function($routeProvider, localStorageServiceProvider) {
+        localStorageServiceProvider.setPrefix('la1tv');
+
+        $routeProvider          
+            .when("/social-media", {
+                templateUrl: '/admin/templates/social-media.tmpl.html',
+                controller: 'socialmediaCGController'
+            })
+            .otherwise({redirectTo: '/social-media'});
+    }
+]);
+
+
+app.controller('socialmediaCGController', ['$scope', 'socket',
+    function($scope, socket) {
+        socket.on("socialmedia", function (msg) {
+            $scope.socialmedia = msg;
+            $scope.socialmedia.scale = Number($scope.socialmedia.scalepc) / 100;
+			
+        });
+
+        $scope.$watch('socialmedia', function() {
+            if ($scope.socialmedia) {
+                socket.emit("socialmedia", $scope.socialmedia);
+            } else {
+                getSocialMediaData();
+            }
+        }, true);
+
+        function getSocialMediaData() {
+            socket.emit("socialmedia:get");
+        }
+
+    }
+]);
+
---
+++ server.js
@@ -0,0 +1,43 @@
+var express 	= require('express'),
+	http 		= require('http'),
+	Stopwatch 	= require('./models/stopwatch');
+
+var app = express();
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+
+var socialmedia = {tweet: "", image:"largelogo", pos: "top center", tweethtml: "", scale: 100};
+
+//Clock Functions
+var stopwatch = new Stopwatch();
+
+stopwatch.on('tick:stopwatch', function(time) {
+	io.sockets.emit("clock:tick", time);
+});
+
+
+
+io.on('connection', function(socket) {
+	console.log("Client Socket Connected");
+	
+    /*
+	 * 		Social Media
+	 */
+	socket.on("socialmedia", function(msg) {
+        socialmedia = msg;
+		io.sockets.emit("socialmedia", msg);
+	});
+
+    socket.on("socialmedia:get", function(msg) {
+        io.sockets.emit("socialmedia", socialmedia);
+    });
+
+
+});
+
+//Serve the puplic dir
+app.use(express.static(__dirname + "/public"));
+
+server.listen(3000);
+console.log("Now listening on port 3000. Go to http://127.0.0.1:3000/admin to control")
+console.log("run 'play 1-1 [html] http://127.0.0.1:3000' in CasparCG to start the graphics")
